f7dc623bf2b6ec81fa78e489414969901973390f
bgoerdt <benjamin-goerdt@uiowa.edu>
2013-02-26 02:00:23 -0600

    check boxes now appear correctly

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,13 @@ class Movie < ActiveRecord::Base
   end
 
   def self.get_all_ratings
-    Movie.find_all_ratings
+    ratings = Array.new
+    Movie.all.each do |m| 
+      if !ratings.include?("#{m.rating}")
+        ratings << "#{m.rating}"
+      end
+    end
+    return ratings
   end
+
 end
